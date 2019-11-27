85095b2222ddd20b6709f504b6c6687dd68b158b
Matt Hamlin <matthewjameshamlin@gmail.com>
2019-11-27 10:55:57 -0800

    Kitchen sink won't work with yarn 2 yet

--- packages/kitchen-sink/src/index.js
+++ packages/kitchen-sink/src/index.js
@@ -1,7 +1,7 @@
-import React from 'react'
 import { BrowserRouter } from '@hamlim/reroute-browser'
 import { ThemeProvider } from '@hamlim/component-library'
 import { createRoot } from 'react-dom'
+import React from 'react'
 import Landing from './Landing'
 
 createRoot(document.querySelector('#root')).render(
