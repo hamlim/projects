20a2d73bc6994a191b086cbe82387cb8ead4525d
Matt Hamlin <matthewjameshamlin@gmail.com>
2019-12-07 21:27:52 -0500

    Never cache babel config maybe

--- packages/component-library/babel.config.js
+++ packages/component-library/babel.config.js
@@ -1,5 +1,5 @@
 module.exports = function(api) {
-  api.cache.using(() => process.env.NODE_ENV)
+  api.cache.never()
   let cfg = '@babel/preset-env'
   if (process.env.NODE_ENV === 'test') {
     cfg = [
