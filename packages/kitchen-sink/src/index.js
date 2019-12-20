13e13fa7980a8fbf1b7e6909ec085f3292802b13
Matt Hamlin <matthewjameshamlin@gmail.com>
2019-12-20 16:38:19 -0500

    Remove cache, get it working

--- packages/kitchen-sink/src/index.js
+++ packages/kitchen-sink/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { BrowserRouter } from '@matthamlin/reroute-browser'
-import { ThemeProvider } from '@matthamlin/component-library'
+import { ThemeProvider, GlobalStyles } from '@matthamlin/component-library'
 import { createRoot } from 'react-dom'
 import Landing from './Landing'
 
 createRoot(document.querySelector('#root')).render(
   <ThemeProvider>
+    <GlobalStyles />
     <BrowserRouter>
       <Landing />
     </BrowserRouter>
