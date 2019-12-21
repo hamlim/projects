67ef7324c874f5ea91736f7fc2723643f38bc8bb
Matt Hamlin <matthewjameshamlin@gmail.com>
2019-12-21 06:16:49 -0500

    Attempt to fix theme bug

--- packages/tasks/src/index.js
+++ packages/tasks/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import { BrowserRouter } from '@matthamlin/reroute-browser'
-import { ThemeProvider, GlobalStyles } from '@matthamlin/component-library'
+import {
+  ThemeProvider,
+  GlobalStyles,
+  H1,
+  Button,
+} from '@matthamlin/component-library'
 import { createRoot } from 'react-dom'
 
 createRoot(document.querySelector('#root')).render(
   <ThemeProvider>
     <GlobalStyles />
     <BrowserRouter>
-      <div>TODO</div>
+      <H1>TODO</H1>
+      <Button>Test</Button>
     </BrowserRouter>
   </ThemeProvider>,
 )
