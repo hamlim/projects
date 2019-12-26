928f57bb40dfac8967642c6c7c8bf466ccd870c7
Matt Hamlin <matthewjameshamlin@gmail.com>
2019-12-26 10:49:19 -0800

    Abstract over the style reset

--- packages/component-library/src/List.js
+++ packages/component-library/src/List.js
@@ -2,10 +2,9 @@ import React from 'react'
 import { css } from 'styled-components'
 import { Box } from './Box'
 
-export function OrderedList(props) {
+function Abstract(props) {
   return (
     <Box
-      as="ol"
       css={css`
         list-style: none;
         margin-bottom: 0;
@@ -22,24 +21,12 @@ export function OrderedList(props) {
   )
 }
 
+export function OrderedList(props) {
+  return <Abstract as="ol" {...props} />
+}
+
 export function UnorderedList(props) {
-  return (
-    <Box
-      as="ul"
-      css={css`
-        list-style: none;
-        margin-bottom: 0;
-        margin-left: 0;
-        margin-right: 0;
-        margin-top: 0;
-        padding-bottom: 0;
-        padding-left: 0;
-        padding-right: 0;
-        padding-top: 0;
-      `}
-      {...props}
-    />
-  )
+  return <Abstract as="ul" {...props} />
 }
 
 export function ListItem(props) {
