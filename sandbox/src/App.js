1d974708be53e7a37afdadc13c7043ef57c9e05a
Matt Hamlin <matthewjameshamlin@gmail.com>
2020-02-16 10:13:44 -0500

    Implement some radio options to test out the components

--- sandbox/src/App.js
+++ sandbox/src/App.js
@@ -1,5 +1,34 @@
-import React from 'react'
-import { ThemeProvider, Button, Banner } from '@matthamlin/component-library'
+import React, { useContext } from 'react'
+import {
+  ThemeProvider,
+  Button,
+  Banner,
+  ControlledFieldset,
+  ControlledRadioButton,
+  fieldsetContext,
+  Box,
+} from '@matthamlin/component-library'
+
+function handleChange(val) {
+  console.log(val)
+}
+
+function Radio({ value, disabled }) {
+  let [selectedValue] = useContext(fieldsetContext)
+
+  return (
+    <label
+      style={{
+        display: 'block',
+        outline: selectedValue === value ? 'solid 1px mediumseagreen' : 'none',
+        padding: 20,
+      }}
+    >
+      <ControlledRadioButton disabled={disabled} value={value} />{' '}
+      {value.toUpperCase()}
+    </label>
+  )
+}
 
 function App() {
   return (
@@ -7,6 +36,13 @@ function App() {
       <div className="App">
         <Button onTap={() => console.log('Tap')}>Click Me</Button>
         <Banner variant="success">Banner</Banner>
+        <ControlledFieldset name="foo" defaultValue="a" onChange={handleChange}>
+          <Box m={4}>
+            <Radio value="a" />
+            <Radio value="b" />
+            <Radio value="c" />
+          </Box>
+        </ControlledFieldset>
       </div>
     </ThemeProvider>
   )
