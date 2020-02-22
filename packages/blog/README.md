5c25d0fd2826a0c158fafe12d9b338eb9288c39d
Matt Hamlin <matthewjameshamlin@gmail.com>
2020-02-22 13:16:53 -0500

    Adding a new blog post (#45)
    * Add new blog post
    
    * Deploys to now

--- packages/blog/README.md
+++ packages/blog/README.md
@@ -5,3 +5,9 @@ TODO
 ## Running
 
 - yw watch
+
+## Adding a Post
+
+- Create mdx file in the `src/posts/` directory
+- Run `yarn update-posts`
+- Deploy
--- packages/blog/babel.config.js
+++ packages/blog/babel.config.js
@@ -2,5 +2,6 @@ module.exports = function(api) {
   api.cache.never()
   return {
     presets: ['@babel/preset-env', '@babel/preset-react'],
+    plugins: ['babel-plugin-styled-components'],
   }
 }
