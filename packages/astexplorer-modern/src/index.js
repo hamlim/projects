5896e4182b95d551b64893b949843fbf53b2a94d
Matt Hamlin <matthewjameshamlin@gmail.com>
2020-04-15 17:40:32 -0400

    Styles, add parser output

--- packages/astexplorer-modern/src/index.js
+++ packages/astexplorer-modern/src/index.js
@@ -1,10 +1,14 @@
-import React, { useRef, useEffect } from 'react'
+import * as React from 'react'
 import { BrowserRouter } from '@matthamlin/reroute-browser'
 import { ThemeProvider, Box } from '@matthamlin/component-library'
 import { createRoot } from 'react-dom'
+import ErrorBoundary from '@matthamlin/error-boundary'
 
 import * as monaco from 'monaco-editor'
 
+import * as parser from '@babel/parser'
+
+// handle loading workers
 self.MonacoEnvironment = {
   getWorkerUrl: function(moduleId, label) {
     if (label === 'json') {
@@ -23,9 +27,15 @@ self.MonacoEnvironment = {
   },
 }
 
-function Editor({ value, onChange, language = 'javascript' }) {
+let { useRef, useEffect, useState } = React
+
+function Editor({
+  value,
+  onChange,
+  language = 'javascript',
+  minHeight = '100vh',
+}) {
   let editorEl = useRef()
-  let monacoInstance = useRef()
   useEffect(() => {
     if (editorEl.current) {
       let editor = monaco.editor.create(editorEl.current, {
@@ -33,28 +43,98 @@ function Editor({ value, onChange, language = 'javascript' }) {
         language,
       })
 
-      monacoInstance.current = editor
+      editor.onDidChangeModelContent(evt => {
+        onChange(editor.getValue())
+      })
     }
   }, [])
 
-  return <div ref={editorEl} style={{ minHeight: '100vh' }} />
+  return <div ref={editorEl} style={{ minHeight }} />
 }
 
-createRoot(document.querySelector('#root')).render(
-  <ThemeProvider>
-    <BrowserRouter>
-      <Box display="grid" gridTemplateColumns="1fr 1fr">
-        <Box style={{ resize: 'both' }} border="solid 1px">
-          <Editor
-            value={`console.log('foo');
+let parserOpts = {
+  sourceType: 'module',
+  allowImportExportEverywhere: false,
+  allowReturnOutsideFunction: false,
+  ranges: false,
+  tokens: false,
+  plugins: [
+    'asyncGenerators',
+    'classProperties',
+    ['decorators', { decoratorsBeforeExport: false }],
+    'doExpressions',
+    'exportExtensions',
+    'flow',
+    'functionSent',
+    'functionBind',
+    'jsx',
+    'objectRestSpread',
+    'dynamicImport',
+    'nullishCoalescingOperator',
+    'numericSeparator',
+    'optionalChaining',
+    'optionalCatchBinding',
+  ],
+}
+
+function ASTPreview({ value }) {
+  return (
+    <Editor
+      value={JSON.stringify(parser.parse(value, parserOpts), null, 2)}
+      language="json"
+    />
+  )
+}
+
+function App() {
+  let [value, setValue] = useState(`console.log('foo');
       
 export default function Foo() {
   return <div />
-}`}
-          />
+}`)
+  let [
+    transform,
+    setTransform,
+  ] = useState(`export default function createPlugin(babel) {
+  let {types: t} = babel;
+  return {
+    visitor: {
+      CallExpression(path) {
+        console.log(path);
+      }
+    }
+  }
+}`)
+
+  return (
+    <ThemeProvider>
+      <BrowserRouter>
+        <Box display="grid" flexGrow={1} gridTemplateColumns="1fr 1fr">
+          <Box style={{ resize: 'both' }} border="solid 1px">
+            <Editor value={value} onChange={setValue} minHeight="50vh" />
+            <Editor
+              value={transform}
+              onChange={setTransform}
+              minHeight="50vh"
+            />
+          </Box>
+          <Box style={{ resize: 'both' }} border="solid 1px">
+            <ErrorBoundary
+              key={value}
+              Fallback={({ error }) => (
+                <Editor
+                  value={`${error.message}\n${error.stack}`}
+                  language=""
+                />
+              )}
+            >
+              <ASTPreview value={value} />
+            </ErrorBoundary>
+          </Box>
         </Box>
-        <Box style={{ resize: 'both' }} border="solid 1px" />
-      </Box>
-    </BrowserRouter>
-  </ThemeProvider>,
-)
+      </BrowserRouter>
+    </ThemeProvider>
+  )
+}
+
+createRoot(document.querySelector('#root')).render(<App />)
