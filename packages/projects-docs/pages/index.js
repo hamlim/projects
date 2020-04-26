26927828eaca7014d27be9884b72e1299791a118
Matt Hamlin <matthewjameshamlin@gmail.com>
2020-04-26 12:48:31 -0400

    WIP routing, but its broken for some reason

--- packages/projects-docs/pages/index.js
+++ packages/projects-docs/pages/index.js
@@ -1,7 +1,8 @@
 import * as React from 'react'
 import Head from 'next/head'
-import { ThemeProvider } from '@matthamlin/component-library'
+import { ThemeProvider, Link } from '@matthamlin/component-library'
 import ErrorBoundary from '@matthamlin/error-boundary'
+import NextLink from 'next/link'
 
 function Fallback() {
   return <p>Error Encountered</p>
@@ -14,6 +15,10 @@ export default function Home() {
         <Head>
           <title>Projects Monorepo</title>
         </Head>
+
+        <NextLink href="/component-library">
+          <Link>@matthamlin/component-library</Link>
+        </NextLink>
       </ErrorBoundary>
     </ThemeProvider>
   )
